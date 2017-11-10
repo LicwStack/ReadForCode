f00c30f42065f910cc54a3019db7fa71b98ce0d5
lichunwei <LicwStack@163.com>
2017-11-10 09:59:05 +0800

    LeetCode

---
+++ C#/LeetCode_TwoSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace leetcode
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var solution = new Solution();
+            var arr = new[] { 2, 7, 11, 15 };
+
+            foreach (var i in solution.TwoSum(arr, 9))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var arr = new int[2];
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] != target) continue;
+                    arr[0] = i;
+                    arr[1] = j;
+                }
+            }
+            return arr;
+        }
+    }
+}
---
+++ C#/较平均分配.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+
+// 平均分配
+namespace ConsoleApplication1
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            foreach (var test in OptimumAllocation(9, 4))
+            {
+                Console.WriteLine(test);
+
+            }
+            Console.ReadKey();
+        }
+
+        private static ArrayList OptimumAllocation(int total, int number)
+        {
+            var returnArrayList = new ArrayList();
+            var average = total / number;
+            var remainder = total % number;
+
+            for (var i = 0; i < number; i++)
+            {
+                if (i < remainder)
+                {
+                    returnArrayList.Add(average + 1);
+                }
+                else
+                {
+                    returnArrayList.Add(average);
+                }
+            }
+            return returnArrayList;
+        }
+    }
+}
